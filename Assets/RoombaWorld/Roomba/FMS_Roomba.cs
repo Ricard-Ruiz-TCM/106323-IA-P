ce2e9ba818f1dfa2d632ed6e39129f289e7a7b1c
RicardRuiz <rruizc@edu.tecnocampus.cat>
2023-03-10 17:31:37 +0100

    Creación total del EmergyState que equivale a cargar el roomba

--- Assets/RoombaWorld/Roomba/FMS_Roomba.cs
+++ Assets/RoombaWorld/Roomba/FMS_Roomba.cs
@@ -1,4 +1,5 @@
 using FSMs;
+using Steerings;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "FMS_Roomba", menuName = "Finite State Machines/FMS_Roomba", order = 1)]
@@ -7,11 +8,34 @@ public class FMS_Roomba : FiniteStateMachine {
     /** Blackboard */
     private ROOMBA_Blackboard blackboard;
 
+    /** Variables */
+    private GoToTarget goToTarget;
+    private PathFollowing pathFollowing;
+
+    private float maxSpeed;
+    private float maxAcceleration;
+    private GameObject theChargePoint;
+    private float baseWayPointReachedRadius;
+
+    /** SteeringContext */
+    private SteeringContext context;
+
+
+
     /** OnEnter */
     public override void OnEnter() {
 
         /** GetComponent */
         blackboard = GetComponent<ROOMBA_Blackboard>();
+
+        goToTarget = GetComponent<GoToTarget>();
+        pathFollowing = GetComponent<PathFollowing>();
+
+        context = GetComponent<SteeringContext>();
+
+        baseWayPointReachedRadius = pathFollowing.wayPointReachedRadius;
+        maxSpeed = context.maxSpeed;
+        maxAcceleration = context.maxAcceleration;
 
         /** OnEnter */
         base.OnEnter();
@@ -35,45 +59,40 @@ public class FMS_Roomba : FiniteStateMachine {
         /** ---------------------------------- */
 
         /** States */
-        State findNearestChargeSpot = new State("findNearestChargeSpot",
-            () => { },
-            () => { },
-            () => { }
-        );
+        State findNearestChargeSpot = new State("findNearestChargeSpot", () => { }, () => { }, () => { });
 
         State reachChargeSpot = new State("reachChargeSpot",
-            () => { },
-            () => { },
-            () => { }
-        );
+            () => { goToTarget.target = theChargePoint; },
+            () => { }, () => { });
 
         State charge = new State("charge",
             () => { },
-            () => { },
-            () => { }
-        );
+            () => { blackboard.Recharge(Time.deltaTime); },
+            () => { });
 
         /** Transitions */
         Transition lowBatteryDetected = new Transition("lowBatteryDetected",
-            () => { return false; },
-            () => { }
-        );
+            () => { return blackboard.currentCharge <= blackboard.minCharge; },
+            () => { 
+                // Set the base speeds and reached radius variables
+                context.maxSpeed = maxSpeed;
+                context.maxAcceleration = maxAcceleration; 
+                pathFollowing.wayPointReachedRadius = blackboard.chargingStationReachedRadius;
+            });
 
         Transition chargeSpotFound = new Transition("chargeSpotFound",
-            () => { return false; },
-            () => { }
-        );
+            () => {
+                theChargePoint = SensingUtils.FindInstanceWithinRadius(gameObject, "ENERGY", blackboard.chargingStationDetectionRadius);
+                return theChargePoint != null;
+            }, () => { });
 
         Transition chargeSpotReached = new Transition("chargeSpotReached",
-            () => { return false; },
-            () => { }
-        );
+            () => { return goToTarget.routeTerminated(); },
+            () => { });
 
         Transition chargeCompleted = new Transition("chargeCompleted",
-            () => { return false; },
-            () => { }
-        );
-
+            () => { return blackboard.currentCharge >= blackboard.maxCharge; },
+            () => { pathFollowing.wayPointReachedRadius = baseWayPointReachedRadius; });
 
         /** FSM Set Up */
         AddStates(roombaBehaviour, findNearestChargeSpot, reachChargeSpot, charge);
